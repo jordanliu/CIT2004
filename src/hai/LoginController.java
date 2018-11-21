940dc219d2d3869bdc67b38e9ae0bc9abda70f92
Jordan Liu <jordanxavierliu@gmail.com>
2018-11-21 16:15:54 -0500

    Scene Manager

--- src/hai/LoginController.java
+++ src/hai/LoginController.java
@@ -3,12 +3,17 @@ package hai;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class LoginController {
@@ -21,26 +26,53 @@ public class LoginController {
     @FXML
     private ComboBox userType;
 
-
+    //add combobox code
     public void login(ActionEvent event) throws Exception{
         if (usernameField.getText().equals("user") && passwordField.getText().equals("pass")) {
             System.out.println("Login");
-            changeScene();
+
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("student.fxml"));
+            primaryStage.setTitle("HaiOnline");
+            primaryStage.setScene(new Scene(root, 600, 400));
+            primaryStage.setResizable(false);
+            primaryStage.show();
         } else {
             System.out.println("Fail");
             }
         }
 
 
+   /* public User getLoggedUser() {
+        return loggedUser;
+    }
+
+    public boolean userLogging(String userId, String password){
+        if (usernameField.getText().equals("user") && passwordField.getText().equals("pass")) {
+            loggedUser = User.of(userId);
+            gotoProfile();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void userLogout(){
+        loggedUser = null;
+        gotoLogin();
+    }
+
+    private void gotoProfile() {
+        try {
+            replaceSceneContent("staff.fxml");
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
         //add string in arguments
-    public void changeScene() throws Exception{
-        Stage primaryStage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("staff.fxml"));
-        primaryStage.setTitle("HaiOnline");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-     }
+    /**/
     }
 
 
