4aed15d5a832159e0da8e0291f888bb01dd8ac13
jordanliu <jordanxavierliu@gmail.com>
2018-11-22 11:47:08 -0500

    Added error alert for Student

--- src/hai/StudentController.java
+++ src/hai/StudentController.java
@@ -1,8 +1,19 @@
 package hai;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 
 public class StudentController {
 
+    public void exit(ActionEvent event){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you would like to exit?", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.YES) {
+            System.out.println("Exit");
+        }
+    }
 }
