4f5a02fc84e390b40f7704af68897ac513ff89af
Jordan Liu <jordanxavierliu@gmail.com>
2018-11-22 16:03:53 -0500

    Added login redirect

--- src/hai/StaffController.java
+++ src/hai/StaffController.java
@@ -3,9 +3,11 @@ package hai;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 
@@ -28,12 +30,19 @@ public class StaffController {
         System.out.println("View Test");
     }
 
-    public void exit(ActionEvent event){
+    public void exit(ActionEvent event) throws Exception{
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you would like to exit?", ButtonType.YES, ButtonType.NO);
         alert.showAndWait();
 
         if (alert.getResult() == ButtonType.YES) {
-            System.out.println("Exit");
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+            Stage primaryStage = new Stage();
+            primaryStage.getIcons().add(new Image("hai/file/icon.png"));
+            Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+            primaryStage.setTitle("HAIOnline");
+            primaryStage.setScene(new Scene(root, 700, 500));
+            primaryStage.setResizable(false);
+            primaryStage.show();
         }
     }
 }
